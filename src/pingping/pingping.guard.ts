e31199957563a0ae608bcf8e64e61ba6fbf7d240
Luke Hol <lukehol2000@gmail.com>
2020-07-24 14:21:43 +0200

    PingPingGuard: Check for ip address

--- src/pingping/pingping.guard.ts
+++ src/pingping/pingping.guard.ts
@@ -1,11 +1,16 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class PingpingGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+		const request = context.switchToHttp().getRequest();
+		const allowedIp = process.env.PINGPING_API_ALLOWED_IP;
+
+		if (allowedIp && allowedIp !== request.ip) {
+			throw new UnauthorizedException()
+		}
+
     return true;
   }
 }
