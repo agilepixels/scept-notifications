7853d79d75c755a494b0780b44f3e3bf03f61164
Luke Hol <lukehol2000@gmail.com>
2020-07-24 14:21:31 +0200

    PingPingController: Added enable and disable check  routes

--- src/pingping/pingping.controller.ts
+++ src/pingping/pingping.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Param, HttpService, UseGuards } from '@nestjs/common';
+import { PingpingGuard } from './pingping.guard';
 
+@UseGuards(PingpingGuard)
 @Controller('pingping')
-export class PingpingController {}
+export class PingpingController {
+	constructor(
+		private readonly httpService: HttpService,
+	) {}
+
+	@Post(':checkId/disable')
+	async disableCheck(@Param('checkId') checkId: string) {
+		await this.httpService.post(`checks/${checkId}/disable`)
+			.toPromise()
+			.catch(console.error)
+	}
+
+	@Post(':checkId/enable')
+	async enableCheck(@Param('checkId') checkId: string) {
+		await this.httpService.post(`checks/${checkId}/enable`)
+			.toPromise()
+			.catch(console.error)
+	}
+}
