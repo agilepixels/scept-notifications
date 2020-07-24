ade22027a438e3771bd63347bc066282f6701d49
Luke Hol <lukehol2000@gmail.com>
2020-07-24 14:21:11 +0200

    main: Use helmet and trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 const dotenv = require('dotenv');
 
 async function bootstrap() {
 	dotenv.config();
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	app.use(helmet());
+	app.set('trust proxy', 1);
   await app.listen(3000);
 }
 bootstrap();
